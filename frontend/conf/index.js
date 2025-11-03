fb0779fcc6755968d233007bb48332cc99fe5025
Your Name <you@example.com>
2025-11-03 19:54:11 +0530

    initial

--- frontend/conf/index.js
+++ frontend/conf/index.js
@@ -1,4 +1,10 @@
 
-const config = { backendEndpoint: "http://13.203.161.219:8082" };
+// const config = { backendEndpoint: "http://13.203.161.219:8082" };
 
+// export default config;
+
+// const config = { backendEndpoint: "http://localhost:8082" };
+// export default config;
+
+const config = { backendEndpoint: "https://prakhar7543-me-qtripdynamic-2.onrender.com" };
 export default config;
